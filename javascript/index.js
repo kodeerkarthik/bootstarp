8c13b48fd9c4eed96f6c994a6917fa15ea243163
kodeerkarthik <kkrishnegowdaotb1@gmail.com>
2019-06-10 23:01:10 +0530

    aseesment 2 completed using bootstrap and js

--- javascript/index.js
+++ javascript/index.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 300) {
 	    $(".menu").css("background" , "white");
-	    $("a").css("color" , "black");
+	    $(".alink").css("color" , "black");
 	    $(".first-span").css("color" , "#e81050");
 	  }
 
 	  else{
 		  $(".menu").css("background" , "transparent");  	
-		  $("a").css("color" , "white");
+		  $(".alink").css("color" , "white");
 		  $(".first-span").css("color" , "white");
 	  }
   })
